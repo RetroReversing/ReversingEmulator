f40a6df163ccc7e40fa3689ee794b680ef55ca23
A <>
2021-01-03 15:18:19 +0000

    Updated thre mac app

--- run_macosx.sh
+++ run_macosx.sh
@@ -4,12 +4,14 @@ filename=$(basename -- "$GAME")
 extension="${filename##*.}"
 filename="${filename%.*}"
 RA_PATH="/Applications/RetroArch.app/Contents/MacOS/RetroArch"
-CORE="./sameboy_libretro.dylib"
+CORE="sameboy_libretro.dylib"
 if [ "$extension" = "gb" ]; then
-    CORE="./sameboy_libretro.dylib"
+    CORE="sameboy_libretro.dylib"
+    $RA_PATH -L $CORE "$GAME"
 else
     echo "Unknown core for extension: $extension"
 fi
+echo $(pwd)
 echo $extension
-echo $RA_PATH -L $CORE $GAME
-$RA_PATH -L $CORE "$GAME"
+echo $RA_PATH -L $CORE \"$GAME\"
+$RA_PATH -L $CORE \"$GAME\"
