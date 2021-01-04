f8333658f9934d92e5ba73220fb550ff86de35b4
A <>
2021-01-04 14:55:43 +0000

    should now properly load retroarch on windows

--- windows_drop_game_here.bat
+++ windows_drop_game_here.bat
@@ -1,5 +1,7 @@
 REM usage run_windows.bat <filename>
 REM you can also drag and drop a rom here
+setlocal
+cd /d %~dp0
 set _filename=%~n1
 set _extension=%~x1
 echo input file name is ^<%_filename%^> and extension is ^<%_extension%^>
@@ -10,5 +12,6 @@ if "%_extension%"==".gb" (set CORE=sameboy_libretro.%COREEXT%)
 if "%_extension%"==".gg" (set CORE=genesis_plus_gx_libretro.%COREEXT%)
 if "%_extension%"==".sms" (set CORE=genesis_plus_gx_libretro.%COREEXT%)
 set RA_PATH=%userprofile%\AppData\Roaming\RetroArch\retroarch
-%RA_PATH% -v -h -L %CORE% %GAME%
+set libretro_path=%userprofile%\AppData\Roaming\RetroArch\retroarch
+%RA_PATH% -L %~dp0\%CORE% %GAME% --verbose
 pause
